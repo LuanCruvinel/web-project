c32430e37953a76fc60e99938460ae15dd699231
Luan Cruvinel <luancruvinel89@gmail.com>
2023-06-11 22:19:17 -0300

    iniciando o oprojeto

---
+++ src/main/java/com/luan/projectwebfullstackback/ProjectWebFullstackBackApplication.java
@@ -0,0 +1,13 @@
+package com.luan.projectwebfullstackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebFullstackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebFullstackBackApplication.class, args);
+	}
+
+}
