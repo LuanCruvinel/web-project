10cb4de256fab7343856ae263be64486cf78d8a8
Luan Cruvinel <luancruvinel89@gmail.com>
2023-06-14 23:59:23 -0300

    w.i.p

---
+++ src/main/java/com/luan/projectwebfullstackback/tarefa/Tarefa.java
@@ -0,0 +1,90 @@
+package com.luan.projectwebfullstackback.tarefa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Tarefa implements Serializable {
+ private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private  String titulo;
+    private String descricao;
+    private LocalDateTime dataParaFinalizar;
+    private Boolean finalizado = false;
+
+
+    public Tarefa() {
+        super();
+    }
+
+    public Tarefa(Integer id, String titulo, String descricao, LocalDateTime dataParaFinalizar, Boolean finalizado) {
+        super();
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataParaFinalizar = dataParaFinalizar;
+        this.finalizado = finalizado;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getDataParaFinalizar() {
+        return dataParaFinalizar;
+    }
+
+    public void setDataParaFinalizar(LocalDateTime dataParaFinalizar) {
+        this.dataParaFinalizar = dataParaFinalizar;
+    }
+
+    public Boolean getFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(Boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefa = (Tarefa) o;
+        return Objects.equals(id, tarefa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
